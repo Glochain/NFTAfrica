61d17b23d51e9e430a22a5b4b5c4d108db440dc0
Gloria <100801941+Glochain@users.noreply.github.com>
2022-10-08 19:44:59 +0100

    unstoppable domain redirect commit

--- client/src/components/unstoppable.jsx
+++ client/src/components/unstoppable.jsx
@@ -7,20 +7,9 @@ const truncateAddress = (address) => `${address.slice(0, 8)}...${address.slice(-
 
 const uauth = new UAuth({
   clientID: "0e876f9b-988d-40e2-996a-9d941b76994a",
-  redirectUri: "https://nftafrica-glochain.vercel.app/explore",
+  redirectUri: "https://nftafrica-glochain.vercel.app",
   scope: "openid wallet"
-}
-                        
-  /*
-  clientID: "0e876f9b-988d-40e2-996a-9d941b76994a",
-  redirectUri:
-    process.env.NODE_ENV === "production"
-      ? "https://nftafrica-glochain.vercel.app/explore"
-      : "http://localhost:3000",
-}
-*/
-                        
-);
+});
 
 const ConnectWallet = () => {
   const [haveMetamask, sethaveMetamask] = useState(true);
@@ -94,11 +83,11 @@ const ConnectWallet = () => {
             {truncateAddress(address)}
 
           </div>
-          <div>
+          <div className="mt-10 space-x-1 text-2xl sm:text-4xl sm:space-x-3 font-bold tracking-wide py-7 px-10 sm:px-14 border border-orange-500 text-orange-500 bg-white outline-none rounded-l-full rounded-r-full capitalize hover:bg-orange-500 hover:text-red-500 transition-all hover:shadow-orange ">
             <button
-              type="button"
               className="font-2xl tracking-wide py-2 px-5 sm:px-8 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange"
               onClick={logout}
+              type="button"
             >
               Logout
             </button>
